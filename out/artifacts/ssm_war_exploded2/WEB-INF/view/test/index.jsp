27ae370ca118cd9c3351e45cb82e299d3120244f
duguang <123mofang@163.com>
2018-09-26 20:43:31 +0800

    first

---
+++ out/artifacts/ssm_war_exploded2/WEB-INF/view/test/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>index</title>
+</head>
+<body>
+<h2>test's index</h2>
+<br/>
+${test.id}<br/>
+${test.context}<br/>
+${test.viewCount}
+</body>
+</html>
---
+++ src/main/java/com/chenyangsocool/ssm/model/Test.java
@@ -0,0 +1,49 @@
+package com.chenyangsocool.ssm.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value="Test",description="Test")//对类进行swagger注解
+public class Test {
+    @ApiModelProperty(value = "测试id", name = "id")//对类的字段属性进行swagger注解
+    private int id;
+
+    @ApiModelProperty(value = "测试内容", name = "context")
+    private String context;
+
+    @ApiModelProperty(value = "测试内容的浏览数", name = "viewCount")
+    private int viewCount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+
+    public int getViewCount() {
+        return viewCount;
+    }
+
+    public void setViewCount(int viewCount) {
+        this.viewCount = viewCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "id=" + id +
+                ", context='" + context + '\'' +
+                ", viewCount=" + viewCount +
+                '}';
+    }
+}
